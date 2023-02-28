ac318b62bba927bb58fa68eb8cf8944f2f7c03c1
JavaBorka <borka34@gmail.com>
2023-02-28 17:25:18 +0100

    Cviceni 3

--- Lekce6 - du odevzdat/main.js
+++ Lekce6 - du odevzdat/main.js
@@ -29,6 +29,7 @@ console.log(parseDate('03.04.1985'))
 */
 
 // CVIČENÍ 2
+
 // Bez použití knihovny dayjs napište funkci formatDate, která na vstupu obdrží objekt představující datum v následujícím formátu.
 // { day: 12, month: 3, year: 2015 }
 // Funkce vrátí řetězec představující datum ve formátu DD.MM.YYYY.
@@ -55,4 +56,43 @@ const formatDate = (datum) => {
 }
 
 console.log(formatDate({ day: 3, month: 4, year: 2015 }))
+
+// CVIČENÍ 3
+
+// Jak jistě znalci jazyka Python dobře vědí, funkce round v tomto jazyce zaokrouhluje malinko jinak, než jsme zvyklí. Pokud je desetinná část čísla přesně 0.5, Python zaokrouhluje k nejbližšímu sudému číslu. JavaScriptová funkce Math.round naopak provádí zaokrouhlování způsobem, na který jste zvyklí, tedy pro 0.5 vždy nahoru. Někomu by se po po Pythonovském zaokrouhlování mohlo stýskat. Napište proto funkci jménem round, která bude zaokrouhlovat čísla na celé jednotky podle následujících pravidel.
+
+// Pokud je desetinná část menší než 0.5, zaokrouhlujeme dolů.
+
+// Pokud je desetinná část větší než 0.5, zaokrouhlujeme nahoru.
+
+// Pokud je desetinná část přesně rovna 0.5, zaokrouhlujeme k sudému číslu. Tedy například 3.5 se zaokrouhlí na 4, naopak 2.5 se zaokrouhlí na 2.
+
+// V tomto cvičení se vám může hodit funkce Math.trunc, která umí odříznout desetinnou část čísla.
+
+const round = (number) => {
+    const desetina = number - Math.trunc(number);
+    const cele = Math.trunc(number);
+
+    if (desetina < 0.5) {
+        console.log('Zaorouhli smerem dolu')
+        return Math.round(number)
+    }
+    if (desetina > 0.5) {
+        console.log('Zaorouhli smerem nahoru')
+        return Math.round(number)
+    }
+    if (desetina === 0.5) {
+        console.log('Zaokrouhli k nejblizsimu sudemu cislu')
+        const sude = cele/2;
+        const zbytek = sude - Math.trunc(sude);
+
+        if (zbytek === 0) {
+            return number - 0.5
+        } else {
+            return number + 0.5
+        }
+    }
+}
+
+console.log(round(20.5))
 
